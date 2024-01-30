a17950da2ad8814ebda2fbc861cc51fb69c71e71
Roy <roysteve.nc@gmail.com>
2024-01-29 18:53:15 -0600

    fix reports names

--- src/utils.py
+++ src/utils.py
@@ -15,8 +15,8 @@ def save_simple_metrics_report(train_score: float, test_score: float, validation
             report_file.write(f'### {key}: {value.__repr__()}'+'\n')
             
         report_file.write(f'## Train Score: {train_score}'+'\n')
-        report_file.write(f'## Train Score: {test_score}'+'\n')
-        report_file.write(f'## Train Score: {validation_score}'+'\n')
+        report_file.write(f'## Test Score: {test_score}'+'\n')
+        report_file.write(f'## Validation Score: {validation_score}'+'\n')
         
 def get_model_performance_test_set(y_real: pd.Series, y_pred: pd.Series) -> None:
     fig, ax = plt.subplots(figsize=(10,10))
